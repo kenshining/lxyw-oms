815a0188500201d6155e8f4bfdcf3d4a678823fd
KenCheng <kencheng@foxmail.com>
2018-05-03 14:57:48 +0800

    修改用户编辑

--- src/js/sales/customer_edit.js
+++ src/js/sales/customer_edit.js
@@ -7,21 +7,64 @@ layui.config({
         layer = layui.layer,
         element = layui.element;
 
-
-       /* $('#add').on('click', function() {
-            //打开弹出窗口加载内容
-            var index = layer.open({
-                    content: '/sales/sales_edit',
-                    type: 2,
-                    anim: 2, //动画类型
-                    title: '编辑订单',
-                    btn: ['保存', '取消'],
-                    success: function(layero, index){
-                        //console.log(layero, index);
-                    }
-                });
-            //默认全屏显示
-            layer.full(index);
-        });*/
-	
+    $("#addNewConnectorBtn").on('click',function(){
+        //新增联系人
+        addNewConnector();
+    });
 });
+//添加一行联系人
+var addNewConnector = function(){
+
+    var newItem = '<tr>'+
+                      '<td><input type="input" class="layui-input"></td>'+
+                      '<td><input type="input" class="layui-input"></td>'+
+                      '<td nowrap="nowrap"><input type="input" class="layui-input"></td>'+
+                       '<td nowrap="nowrap">'+
+                        '<div class="layui-btn-group">'+
+                           ' <button class="layui-btn layui-btn-primary layui-btn-mini" type="button" onclick="saveConnector(this);">'+
+                              '<i class="layui-icon">&#xe618;保存</i>'+
+                            '</button>'+
+                            '<button class="layui-btn layui-btn-primary layui-btn-mini" type="button" onclick="deleteConnector(this);">'+
+                              '<i class="layui-icon">&#xe640;删除</i>'+
+                            '</button>'+
+                          '</div>'+
+                       '</td>'+
+                   ' </tr>';
+    $("#con_body").append(newItem);
+}
+var listedConnector = function(jsonlist){
+    var items = '<tr>'+
+                      '<td><span class="note">成龙</span></td>'+
+                      '<td><span class="note">18611699091</span></td>'+
+                      '<td nowrap="nowrap"><span class="note">kencheng@foxmail.com</span></td>'+
+                       '<td nowrap="nowrap">'+
+                        '<div class="layui-btn-group">'+
+                           ' <button class="layui-btn layui-btn-primary layui-btn-mini" type="button" onclick="editConnector(this);">'+
+                              '<i class="layui-icon">&#xe642;编辑</i>'+
+                            '</button>'+
+                            '<button class="layui-btn layui-btn-primary layui-btn-mini" type="button" onclick="deleteConnector(this);">'+
+                              '<i class="layui-icon">&#xe640;删除</i>'+
+                            '</button>'+
+                          '</div>'+
+                       '</td>'+
+                   ' </tr>';
+    $("#con_body").append(items);
+}
+//保存联系人
+var saveConnector = function(saveBtn){
+   $("#con_body").find(".layui-input").each(function(){
+        //获取文本框值
+        var value = $(this).val();
+        //处理成不可编辑状态
+        $(this).parent().html('<span class="note">'+value+'</span>');
+  });
+   //变更按钮为编辑和删除
+}
+//删除联系人
+var deleteConnector = function(deleteBtn){
+    $(deleteBtn).parent().parent().parent().remove();
+}
+//编辑联系人
+var editConnector = function(editBtn){
+    $(editBtn).parent().parent().parent().find(".layui-input");
+}

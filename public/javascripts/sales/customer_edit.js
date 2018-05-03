b6e3ec450908141cb1f4eafbbf97a436ff346a54
KenCheng <kencheng@foxmail.com>
2018-05-03 15:28:54 +0800

    为待编辑行增加名称

--- public/javascripts/sales/customer_edit.js
+++ public/javascripts/sales/customer_edit.js
@@ -14,8 +14,12 @@ layui.config({
 });
 //添加一行联系人
 var addNewConnector = function(){
-
-    var newItem = '<tr>'+
+    var i = $("#con_body").find(".layui-input").length;
+    if(i>0){
+        layer.msg("请将待编辑联系人【保存】或【删除】后再编辑数据。");
+        return;
+    }
+    var newItem = '<tr name="connectorItems">'+
                       '<td><input type="input" class="layui-input"></td>'+
                       '<td><input type="input" class="layui-input"></td>'+
                       '<td nowrap="nowrap"><input type="input" class="layui-input"></td>'+
@@ -33,7 +37,7 @@ var addNewConnector = function(){
     $("#con_body").append(newItem);
 }
 var listedConnector = function(jsonlist){
-    var items = '<tr>'+
+    var items = '<tr name="connectorItems">'+
                       '<td><span class="note">成龙</span></td>'+
                       '<td><span class="note">18611699091</span></td>'+
                       '<td nowrap="nowrap"><span class="note">kencheng@foxmail.com</span></td>'+
@@ -59,6 +63,13 @@ var saveConnector = function(saveBtn){
         $(this).parent().html('<span class="note">'+value+'</span>');
   });
    //变更按钮为编辑和删除
+   var btnHtml = ' <button class="layui-btn layui-btn-primary layui-btn-mini" type="button" onclick="editConnector(this);">'+
+                      '<i class="layui-icon">&#xe642;编辑</i>'+
+                    '</button>'+
+                    '<button class="layui-btn layui-btn-primary layui-btn-mini" type="button" onclick="deleteConnector(this);">'+
+                      '<i class="layui-icon">&#xe640;删除</i>'+
+                    '</button>';
+   $(saveBtn).parent().html(btnHtml);
 }
 //删除联系人
 var deleteConnector = function(deleteBtn){
@@ -66,5 +77,23 @@ var deleteConnector = function(deleteBtn){
 }
 //编辑联系人
 var editConnector = function(editBtn){
-    $(editBtn).parent().parent().parent().find(".layui-input");
+    //点编辑之前先判断是否存在新增的数据未保存。
+    var i = $("#con_body").find(".layui-input").length;
+    if(i>0){
+        layer.msg("请将联系人【保存】或【删除】后再编辑数据。");
+        return;
+    }
+    $(editBtn).parent().parent().parent().find(".note").each(function(){
+        var text = $(this).html();
+        $(this).parent().html('<input type="input" class="layui-input" value="'+text+'">'); 
+    });
+    //console.log($(editBtn).parent().parent().parent().find(".note"));
+     //变更按钮为编辑和删除
+   var btnHtml = ' <button class="layui-btn layui-btn-primary layui-btn-mini" type="button" onclick="saveConnector(this);">'+
+                      '<i class="layui-icon">&#xe618;保存</i>'+
+                    '</button>'+
+                    '<button class="layui-btn layui-btn-primary layui-btn-mini" type="button" onclick="deleteConnector(this);">'+
+                      '<i class="layui-icon">&#xe640;删除</i>'+
+                    '</button>';
+   $(editBtn).parent().html(btnHtml);
 }

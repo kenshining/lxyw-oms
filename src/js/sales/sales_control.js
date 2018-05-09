129f8f1131af062656ce2925f5fcaa1939fabdb4
KenCheng <kencheng@foxmail.com>
2018-05-09 17:01:47 +0800

    增加订单TOOLBAR

--- src/js/sales/sales_control.js
+++ src/js/sales/sales_control.js
@@ -40,5 +40,19 @@ layui.use(['form','layer','element','jquery','table'], function() {
             ,loading:true
             ,limits:[10,20,50,90]
         });
+
+        table.on('tool(table_content)', function(obj){
+        var data = obj.data; //获得当前行数据
+        var layEvent = obj.event; //获得 lay-event 对应的值（也可以是表头的 event 参数对应的值）
+        var tr = obj.tr; //获得当前行 tr 的DOM对象
+        //console.log(data);
+        if(layEvent === 'editOrderbtn'){ //编辑
+            
+        }else if(layEvent === 'editOrderbtn'){//删除
+
+        }else{//订单履历
+
+        }
+  });
 	
 });
